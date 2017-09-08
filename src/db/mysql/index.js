0a6c9cd0a864ed1dea5be79c217b8f0868728211
Thiago Moreira <thiago.cmoreira@yahoo.com.br>
2017-09-08 04:26:18 -0300

    :card_file_box: Adding support to .env file and example

--- src/db/mysql/index.js
+++ src/db/mysql/index.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql')
 
 var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'restful_ws'
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
 })
 
 connection.connect()
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const server = require('./server/index')
 
 server.listen('8080')
