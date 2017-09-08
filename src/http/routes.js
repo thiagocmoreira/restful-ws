f76125a85bc249c70ded507afd8fcad2eeb4e41f
Thiago Moreira <thiago.cmoreira@yahoo.com.br>
2017-09-08 01:24:19 -0300

    :art: Adding simple routes to application

--- src/http/routes.js
+++ src/http/routes.js
@@ -3,6 +3,26 @@ const routes = (server) => {
     res.send('Welcome!')
     next()
   })
+
+  server.get('category', (req, res, next) => {
+    res.send([1, 'Test'])
+    next()
+  })
+
+  server.post('category', (req, res, next) => {
+    const { name } = req.params
+    res.send(name)
+    next()
+  })
+
+  // server.put('category', (req, res, next) => {
+  //   res.send()
+  //   next()
+  // })
+  // server.delete('category', (req, res, next) => {
+  //   res.send()
+  //   next()
+  // })
 }
 
 module.exports = routes
