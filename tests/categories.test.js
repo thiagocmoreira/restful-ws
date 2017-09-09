7e46905a67dfe3c50fec484c7c88a98650447674
Thiago Moreira <thiago.cmoreira@yahoo.com.br>
2017-09-09 03:04:05 -0300

    :white_check_mark: Adding success tests to categories

--- tests/categories.test.js
+++ tests/categories.test.js
@@ -4,7 +4,29 @@ const categories = require('../src/db/mysql/categories')({ connection, errorHand
 test.beforeEach(t => connection.query('TRUNCATE TABLE categories'))
 test.after.always(t => connection.query('TRUNCATE TABLE categories'))
 
+const create = () => categories.save('Category Test')
+
+// Success paths
+
+test('List Categories', async t => {
+  await create()
+  const result = await categories.all()
+  t.is(result.categories.length, 1)
+})
+
 test('Create Category', async t => {
-  const result = await categories.save('Category Test')
+  const result = await create()
   t.is(result.category.name, 'Category Test')
+})
+
+test('Edit Category', async t => {
+  await create()
+  const result = await categories.update(1, 'Category Edited')
+  t.is(result.category.name, 'Category Edited')
+})
+
+test('Delete Category', async t => {
+  await create()
+  const result = await categories.del(1)
+  t.is(result.category.id, 1)
 })
