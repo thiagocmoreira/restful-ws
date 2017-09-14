6c8d11c5791c404506c1ce44358367018abdea24
Thiago Moreira <thiago.cmoreira@yahoo.com.br>
2017-09-14 01:44:46 -0300

    :sparkles: Creating user api methods

---
+++ src/db/mysql/users.js
@@ -0,0 +1,54 @@
+const user = deps => {
+  return {
+    all: () => {
+      return new Promise((resolve, reject) => {
+        const { connection, errorHandler } = deps
+        connection.query('SELECT id, email FROM user;', (error, results) => {
+          if (!error) {
+            resolve({ users: results })
+          } else {
+            errorHandler(error, 'Erro ao listar os usuários', reject)
+          }
+        })
+      })
+    },
+    save: (email, password) => {
+      return new Promise((resolve, reject) => {
+        const { connection, errorHandler } = deps
+        connection.query('INSERT INTO user (email, password) VALUES (?, ?)', [email, password], (error, results) => {
+          if (!error) {
+            resolve({ user: { id: results.insertId, email, password } })
+          } else {
+            errorHandler(error, `Erro ao salvar o usuário de email ${email}`, reject)
+          }
+        })
+      })
+    },
+    update: (id, password) => {
+      return new Promise((resolve, reject) => {
+        const { connection, errorHandler } = deps
+        connection.query('UPDATE user SET password = ? WHERE id = ?', [password, id], (error, results) => {
+          if (!error) {
+            resolve({ user: { id } })
+          } else {
+            errorHandler(error, `Erro ao atualizar o usuário de ${id}`, reject)
+          }
+        })
+      })
+    },
+    del: (id) => {
+      return new Promise((resolve, reject) => {
+        const { connection, errorHandler } = deps
+        connection.query('DELETE FROM user WHERE id = ?', [id], (error, results) => {
+          if (!error) {
+            resolve({ user: { id: id } })
+          } else {
+            errorHandler(error, `Erro ao atualizar o usuário de id ${id}`, reject)
+          }
+        })
+      })
+    }
+  }
+}
+
+module.exports = user
